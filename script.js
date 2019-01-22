29f879e6d848f8c40065cfbbcf28c5d4f45daf14
Kern Elder <kelel1@yahoo.com>
2019-01-21 19:42:59 -0500

    add operation functions

---
+++ script.js
@@ -0,0 +1,120 @@
+const keypad = document.querySelector("#digits");
+let a;
+let b;
+// var sev = document.getElementById("seven");
+
+// sev.addEventListener("click", displayValue);
+
+
+/** Create calculator keypad */
+function displayKeys() {
+    
+    const keys = document.querySelectorAll('button');
+   
+    for (i = 0; i < keys.length; i++) {        
+        keys[i].setAttribute("style", "border-radius: 25%; height: 35px; width: 35px;" + " "
+        + "background-color: royalblue");         
+    }
+
+    keys[0].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += "7";
+    });
+
+    keys[1].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += "8";
+    });
+
+    keys[2].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += "9";
+    });
+
+    keys[3].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += "4";
+    });
+
+    keys[4].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += "5";
+    });
+
+    keys[5].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += "6";
+    });
+
+    keys[6].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += "1";
+    });
+
+    keys[7].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += "2";
+    });
+
+    keys[8].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += "3";
+    });
+
+    keys[9].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += "0";
+    });
+
+    keys[10].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += ".";
+    });
+
+    keys[11].addEventListener("click", vsalz = () => {
+
+        return document.getElementById("display").innerHTML += "-";
+    });
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+
+
+
+// const keyz = document.getElementById("digits");
+// function setEventListener () {
+
+//     for (i = 0; i < keyz.length; i++) {
+
+//         keyz[i].addEventListener('click', (play) => {
+
+//             displayValue();
+//         });
+//     };
+
+// }
+
+/** Diplays key value, when clicked */
+// function displayValue() {
+//     document.getElementById("display").innerHTML += "7";
+
+//     // switch (document.getElementById("display"))
+// }
+displayKeys();
+
+console.log(divide(8,2.789));
